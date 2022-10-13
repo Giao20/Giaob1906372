aea6cafa58ab11560a5ad592904104d5787a744d
Giao20 <115712613+Giao20@users.noreply.github.com>
2022-10-13 20:35:12 +0700

    Add files via upload

---
+++ formnhap.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="luu.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Birthday: <input type="date" name="birth"><br>
+<label for="major">Chuyên ngành:</label>
+
+<select id="major">
+  <option value="ktpm">Kỹ thuật phần mềm</option>
+  <option value="httt">Hệ thống thông tin</option>
+  <option value="cnnt">Công nghệ thông tin</option>
+  <option value="attt">An toàn thông tin</option>
+</select><br>
+
+<input type="submit" value="Gửi">
+</form>
+
+</body>
+</html>
+
+
+
